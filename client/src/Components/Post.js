f8ce0e2e9ada7183b5788b497916cd5c4eac66a4
Batuhangulmez <batuhanngulmez@gmail.com>
2022-05-10 23:41:30 +0300

    post component css change

--- client/src/Components/Post.js
+++ client/src/Components/Post.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../mystyle.module.css";
 
-import { Card, Button, CloseButton, Modal } from "react-bootstrap";
+import { Card, Button, CloseButton, Modal, Form } from "react-bootstrap";
 
 import { useDispatch } from "react-redux";
 
@@ -13,9 +13,26 @@ const Post = ({ post }) => {
   const dispatch = useDispatch();
 
   const [show, setShow] = useState(false);
+  const [typeSelector, setTypeSelector] = useState("");
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    switch (post.type) {
+      case "Vaka":
+        setTypeSelector("#fd9a8d");
+        break;
+      case "Medical içerik":
+        setTypeSelector("#fead54");
+        break;
+      case "Metin":
+        setTypeSelector("#808080");
+        break;
+      default:
+        break;
+    }
+  }, []);
 
   return (
     <>
@@ -29,12 +46,15 @@ const Post = ({ post }) => {
         <Modal.Header closeButton className={styles.header}>
           <div>
             <img src={Logo} className={styles.img} alt="img" />
-            <text>{post.creator}</text>
+            <div>
+              <text>{post.creator}</text>
+              <span>{post.creator}</span>
+            </div>
           </div>
         </Modal.Header>
         <Modal.Body>
           <Card.Title>
-            <span>{post.category}</span>
+            <article>{post.category}</article>
           </Card.Title>
           <Card.Text>{post.content}</Card.Text>
         </Modal.Body>
@@ -56,19 +76,33 @@ const Post = ({ post }) => {
             <Card.Text>{post.content}</Card.Text>
           </Modal.Body>
         </Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="" controlId="exampleForm.ControlInput1">
+              <Form.Label>Yorum Yaz</Form.Label>
+              <Form.Control type="textarea" placeholder="..." />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
+            İptal
           </Button>
-          <Button variant="primary">Understood</Button>
+          <Button variant="primary">Gönder</Button>
         </Modal.Footer>
       </Modal>
-
-      <Card onClick={handleShow} className={styles.postContent}>
+      <Card
+        onClick={handleShow}
+        style={{ borderColor: typeSelector }}
+        className={styles.postContent}
+      >
         <Card.Header className={styles.header}>
           <div>
             <img src={Logo} className={styles.img} alt="img" />
-            <text>{post.creator}</text>
+            <div>
+              <text>{post.creator}</text>
+              <span>{post.creator}</span>
+            </div>
           </div>
           <CloseButton
             onClick={() => {
@@ -78,7 +112,7 @@ const Post = ({ post }) => {
         </Card.Header>
         <Card.Body>
           <Card.Title>
-            <span>{post.category}</span>
+            <article>{post.category}</article>
           </Card.Title>
           <Card.Text>{post.content}</Card.Text>
         </Card.Body>
